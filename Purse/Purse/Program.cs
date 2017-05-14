eb1931ab437e69cdba1fdb8e2475f7aef82a4e12
NazarKhandoha <khandoha0110nazar@gmail.com>
2017-05-14 18:44:30 +0300

    First commit - add folder Purse

---
+++ Purse/Purse/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Purse
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var money = new Money.Money(100);
+            Console.WriteLine(money.GetMoney());
+            var credit = new CreditCard.CreditCard(500);
+            Console.WriteLine(credit.GetSumCreditCard());
+            var bcard = new BusinessCard.BusinessCard("Iowlit");
+            Console.WriteLine(bcard.FindOutWhose());
+        }
+    }
+}
