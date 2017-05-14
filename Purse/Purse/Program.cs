0843e080f49dffa7fbff0da8d428771cadfdfce3
NazarKhandoha <khandoha0110nazar@gmail.com>
2017-05-14 21:05:59 +0300

    Resolve conflicts

--- Purse/Purse/Program.cs
+++ Purse/Purse/Program.cs
@@ -6,11 +6,13 @@ namespace Purse
     {
         static void Main(string[] args)
         {
+			//In this file we have conflicts
             var money = new Money.Money(100);
             Console.WriteLine(money.GetMoney());
             var credit = new CreditCard.CreditCard(500);
             Console.WriteLine(credit.GetSumCreditCard());
             var bcard = new BusinessCard.BusinessCard("My business Nazar Volodymyrovych  say  ");
+			
             Console.WriteLine(bcard.FindOutWhose());
         }
     }
